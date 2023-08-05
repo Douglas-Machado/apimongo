54bfc0ca78ceda04f2c0cb82c1c6bc3535f8115c
Douglas <douglasclaudino10.dc@gmail.com>
2023-08-05 03:06:49 -0300

    connect mongodb

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User douglas = new User("1", "Douglas Machado", "douglas@hotmail.com");
-		User junior = new User("2", "Junior Santos", "junior@hotmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(douglas, junior));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
